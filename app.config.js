534a40673b997f785c61dc7419c937f320c43da9
Mark <markmurphy561@gmail.com>
2025-07-21 14:12:28 -0500

    - app variants again

--- app.config.js
+++ app.config.js
@@ -25,17 +25,64 @@ const getAppName = () => {
     return 'immersive_experiences_v3';
 };
 
-
-export default ({ config }) => ({
-    ...config,
+export default {
     name: getAppName(),
+    slug: "immersive_experiences_v3",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "immersiveexperiencesv3",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
     ios: {
-        ...config.ios,
+        supportsTablet: true,
         bundleIdentifier: getUniqueIdentifier(),
     },
     android: {
-        ...config.android,
+        adaptiveIcon: {
+            foregroundImage: "./assets/images/adaptive-icon.png",
+            backgroundColor: "#ffffff"
+        },
+        permissions: [
+            "android.permission.ACCESS_COARSE_LOCATION",
+            "android.permission.ACCESS_FINE_LOCATION"
+        ],
+        edgeToEdgeEnabled: true,
         package: getUniqueIdentifier(),
+
     },
-});
+    web: {
+        bundler: "metro",
+        output: "static",
+        favicon: "./assets/images/favicon.png"
+    },
+    plugins: [
+        "expo-router",
+        [
+            "expo-splash-screen",
+            {
+                "image": "./assets/images/splash-icon.png",
+                "imageWidth": 200,
+                "resizeMode": "contain",
+                "backgroundColor": "#ffffff"
+            }
+        ],
+        [
+            "expo-maps",
+            {
+                "requestLocationPermission": true,
+                "locationPermission": "Allow Imers.io to use your location"
+            }
+        ]
+    ],
+    experiments: {
+        typedRoutes: true
+    },
+    extra: {
+        router: {},
+        eas: {
+            projectId: "bb37e65e-925d-4ad0-92db-0952ff9926c1"
+        }
+    }
+};
 
